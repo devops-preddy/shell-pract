fe58f12475c5a5bc3d9e45be148fc3b63103f8b3
sravan <sravanreks@gmail.com>
2026-01-30 11:59:58 +0530

    changes

--- 05-variable-time.sh
+++ 05-variable-time.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Display data and time format
 START_TIME=$(date)
 
 echo "execution time :$START_TIME"
@@ -8,3 +9,16 @@ sleep 10
 
 END_TIME=$(date)
 echo "execution end time:$END_TIME"
+
+#Display time in full numeric format
+
+START_TIME=$(date +%s)
+
+echo "Script executed at: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in: $TOTAL_TIME seconds"
