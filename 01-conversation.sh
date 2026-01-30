6f68d3f39e1c8f329bc0666a8ae315ac311abf9f
sravan <sravanreks@gmail.com>
2026-01-30 11:31:24 +0530

    change

---
+++ 01-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Sravan:: How are your Anil"
+echo "Anil:: I am good Sravan thank for asking , how are you doing"
+echo "Sravan:: I am good Anil , What are you learning"
+echo "Anil:: I am learning shell"
+echo "Sravan:: Good, I am also learning DevOps"
---
+++ 02.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSION1=Sravan
+PERSION2=Anil
+
+echo "$PERSON1:: How are your $PERSON2"
+echo "$PERSON2:: I am good $PERSON1 thank for asking , how are you doing"
+echo "$PERSON1:: I am good $PERSON2 , What are you learning"
+echo "$PERSON2:: I am learning shell"
+echo "$PERSON1:: Good, I am also learning DevOps"
